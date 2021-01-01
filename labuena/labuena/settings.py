c67e1eae2e924daa23e525227f6afa1fdf094234
roronoazor <ugoodumegwu@gmail.com>
2021-01-01 20:30:12 +0100

    modified settings.py

--- labuena/labuena/settings.py
+++ labuena/labuena/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "labuena2.herokuapp.com"]
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
