5f74439964f1da984027fabb1e13608e2ed808e1
roronoazor <32907795+roronoazor@users.noreply.github.com>
2021-01-17 18:05:13 +0100

    Update models.py

--- labuena/core/models.py
+++ labuena/core/models.py
@@ -8,4 +8,4 @@ class WhatsappInfo(models.Model):
     phone_number = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.name
+        return "name: %s, phone nummber: %s" % (self.name, self.phone_number)
