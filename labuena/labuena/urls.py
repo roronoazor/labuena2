62b23ab010a1f591bf6119afc1bc591b1bc78e2a
roronoazor <32907795+roronoazor@users.noreply.github.com>
2021-01-17 17:41:04 +0100

    Update urls.py

--- labuena/labuena/urls.py
+++ labuena/labuena/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("core.urls")),
-    # path('', include("labuena.core.urls")),
+    # path('', include("core.urls")), local 
+    path('', include("labuena.core.urls")),
 ]
